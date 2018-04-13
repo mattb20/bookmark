2363f39c379e689ac80985a64d236efb7166da22
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-13 12:03:00 +0100

    refactored bookmark.

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,47 +2,47 @@ require 'pg'
 
 class Bookmark
   def self.all
-            if ENV['RACK_ENV'] == 'test'
-                   connection = PG.connect(dbname: 'bookmark_manager_test')
-                 else
-                  connection =  PG.connect(dbname: 'bookmark_manager')
-                 end
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection =  PG.connect(dbname: 'bookmark_manager')
+    end
     result = connection.exec('SELECT * FROM bookmarks')
     result.map { |bookmark| bookmark['title'] }
   end
   def self.display
-               if ENV['RACK_ENV'] == 'test'
-                  connection =  PG.connect(dbname: 'bookmark_manager_test')
-                 else
-                   connection = PG.connect(dbname: 'bookmark_manager')
-                 end
+    if ENV['RACK_ENV'] == 'test'
+      connection =  PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
     result = connection.exec('SELECT * FROM bookmarks')
     result.map { |bookmark| "<a href=" + bookmark['url'] + ">" + bookmark['title'] + "</a>" + "<input type='submit' value = Delete" + " name = " + bookmark['title'] + ">" }
   end
 
   def self.create(link, title)
     return false unless is_url?(link) && title?(title)
-               if ENV['RACK_ENV'] == 'test'
-                   connection = PG.connect(dbname: 'bookmark_manager_test')
-                 else
-                   connection =  PG.connect(dbname: 'bookmark_manager')
-                end
-                result = connection.exec('SELECT * FROM bookmarks')
-                url_list = result.map { |bookmark| bookmark['url'] }
-                if url_list.include?(link)
-                  connection.exec "UPDATE bookmarks SET title = '#{title}' WHERE url = '#{link}'"
-                else
-                  connection.exec("INSERT INTO bookmarks(url, title) VALUES('#{link}', '#{title}')")
-                end
+    if ENV['RACK_ENV'] == 'test'
+        connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection =  PG.connect(dbname: 'bookmark_manager')
+    end
+    result = connection.exec('SELECT * FROM bookmarks')
+    url_list = result.map { |bookmark| bookmark['url'] }
+    if url_list.include?(link)
+      connection.exec "UPDATE bookmarks SET title = '#{title}' WHERE url = '#{link}'"
+    else
+      connection.exec("INSERT INTO bookmarks(url, title) VALUES('#{link}', '#{title}')")
+    end
 
-                result = connection.exec('SELECT * FROM bookmarks')
-              end
+    result = connection.exec('SELECT * FROM bookmarks')
+  end
   def self.url
     if ENV['RACK_ENV'] == 'test'
-                   connection = PG.connect(dbname: 'bookmark_manager_test')
-                 else
-                   connection = PG.connect(dbname: 'bookmark_manager')
-                 end
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
     result = connection.exec('SELECT * FROM bookmarks')
     result.map { |bookmark| bookmark['url'] }
   end
